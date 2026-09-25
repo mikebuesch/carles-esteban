6aa496411b238ab4eca2235d5fc20e92b2dc19f0
mikebuesch <mikebuesch@gmail.com>
2026-09-25 11:08:44 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,87 @@
+/*
+  BOOK DATA
+  ----------
+  You can edit this file later when you have the texts from the USB.
+
+  For now, the reading sections are empty.
+*/
+
+const BOOKS = [
+  {
+    id: "book-1",
+    author: "Carles Esteban",
+    title: "Desconocidos",
+    cover: "assets/covers/book-1.png",
+    theme: "#efe4e4",
+    amazon: "https://www.amazon.es/Desconocidos-Carlos-Esteban/dp/B0H9HTM8BR/ref=mp_s_a_1_1?crid=3M5COF67LVJXR&dib=eyJ2IjoiMSJ9.fKmUagLvFzdD8M4pWX3XrFOh6DqyI4CVoh7v1vJaUB14ZoQ_2QuH25baMnIGLWThQ7dM8fvg2qw3dnRcA_kgbYx_IYHsVnOTEJaE_kIirH933itipwVnHr_Uz9U2D5ofUWRXNhsmE4vSaLryKuZk5N247QjnfBTyUhHHxLiYltzk-QONysu_gbaObSMHD5RYyw162k4vY84yc46e6leUww.sjVL8MSooXM6XNUHBslQ0WVz0xLhpEtF_o-JFivz2sc&dib_tag=se&keywords=carlos+esteban&qid=1790324829&sprefix=carlos+esteban%2Caps%2C79&sr=8-1",
+    texts: [
+      {
+        id: "text-1",
+        title: "Texto 1",
+        content: ""
+      },
+      {
+        id: "text-2",
+        title: "Texto 2",
+        content: ""
+      },
+      {
+        id: "text-3",
+        title: "Texto 3",
+        content: ""
+      }
+    ]
+  },
+
+  {
+    id: "book-2",
+    author: "Carles Esteban",
+    title: "La mujer de sus sueños",
+    cover: "assets/covers/book-2.png",
+    theme: "#dceef4",
+    amazon: "https://www.amazon.es/mujer-sus-sue%C3%B1os-Carles-Esteban/dp/B0BQY8V94C/ref=mp_s_a_1_2?crid=3M5COF67LVJXR&dib=eyJ2IjoiMSJ9.fKmUagLvFzdD8M4pWX3XrFOh6DqyI4CVoh7v1vJaUB14ZoQ_2QuH25baMnIGLWThQ7dM8fvg2qw3dnRcA_kgbYx_IYHsVnOTEJaE_kIirH933itipwVnHr_Uz9U2D5ofUWRXNhsmE4vSaLryKuZk5N247QjnfBTyUhHHxLiYltzk-QONysu_gbaObSMHD5RYyw162k4vY84yc46e6leUww.sjVL8MSooXM6XNUHBslQ0WVz0xLhpEtF_o-JFivz2sc&dib_tag=se&keywords=carlos+esteban&qid=1790324829&sprefix=carlos+esteban%2Caps%2C79&sr=8-2",
+    texts: [
+      {
+        id: "text-1",
+        title: "Texto 1",
+        content: ""
+      },
+      {
+        id: "text-2",
+        title: "Texto 2",
+        content: ""
+      },
+      {
+        id: "text-3",
+        title: "Texto 3",
+        content: ""
+      }
+    ]
+  },
+
+  {
+    id: "book-3",
+    author: "Carles Esteban",
+    title: "El espíritu de las ilusiones ocultas",
+    cover: "assets/covers/book-3.png",
+    theme: "#eeeeee",
+    amazon: "https://www.amazon.es/El-esp%C3%ADritu-las-ilusiones-ocultas/dp/B0CSX4D2V3/ref=mp_s_a_1_3?crid=3M5COF67LVJXR&dib=eyJ2IjoiMSJ9.fKmUagLvFzdD8M4pWX3XrFOh6DqyI4CVoh7v1vJaUB14ZoQ_2QuH25baMnIGLWThQ7dM8fvg2qw3dnRcA_kgbYx_IYHsVnOTEJaE_kIirH933itipwVnHr_Uz9U2D5ofUWRXNhsmE4vSaLryKuZk5N247QjnfBTyUhHHxLiYltzk-QONysu_gbaObSMHD5RYyw162k4vY84yc46e6leUww.sjVL8MSooXM6XNUHBslQ0WVz0xLhpEtF_o-JFivz2sc&dib_tag=se&keywords=carlos+esteban&qid=1790324829&sprefix=carlos+esteban%2Caps%2C79&sr=8-3",
+    texts: [
+      {
+        id: "text-1",
+        title: "Texto 1",
+        content: ""
+      },
+      {
+        id: "text-2",
+        title: "Texto 2",
+        content: ""
+      },
+      {
+        id: "text-3",
+        title: "Texto 3",
+        content: ""
+      }
+    ]
+  }
+];
